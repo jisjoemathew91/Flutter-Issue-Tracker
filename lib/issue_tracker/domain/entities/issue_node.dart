eb222ac1599643c5660baca5bad41fcf4fe66ec8
jisjoemathew91 <jisjoemathew91@gmail.com>
2022-07-03 03:21:38 +0530

    Define issue node entity and issues entity

---
+++ lib/issue_tracker/domain/entities/issue_node.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_issue_tracker/issue_tracker/data/model/author_model.dart';
+import 'package:flutter_issue_tracker/issue_tracker/data/model/comments_model.dart';
+import 'package:flutter_issue_tracker/issue_tracker/data/model/labels_model.dart';
+
+class IssueNode {
+  IssueNode({
+    this.id,
+    this.number,
+    this.title,
+    this.author,
+    this.createdAt,
+    this.closedAt,
+    this.body,
+    this.state,
+    this.stateReason,
+    this.comments,
+    this.labels,
+    this.isReadByViewer,
+  });
+
+  String? id;
+  int? number;
+  String? title;
+  AuthorModel? author;
+  String? createdAt;
+  String? closedAt;
+  String? body;
+  String? state;
+  String? stateReason;
+  CommentsModel? comments;
+  LabelsModel? labels;
+  bool? isReadByViewer;
+}
