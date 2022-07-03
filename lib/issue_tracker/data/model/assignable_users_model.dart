03f02a3f55c8371d90a44cda96006bdb742deda1
jisjoemathew91 <jisjoemathew91@gmail.com>
2022-07-03 23:00:23 +0530

    Define assignable users, labels & milestone models

--- lib/issue_tracker/data/model/assignable_users_model.dart
+++ lib/issue_tracker/data/model/assignable_users_model.dart
@@ -1,46 +1,41 @@
-import 'package:flutter_issue_tracker/issue_tracker/data/model/label_node_model.dart';
+import 'package:flutter_issue_tracker/issue_tracker/data/model/assignable_user_node_model.dart';
 import 'package:flutter_issue_tracker/issue_tracker/data/model/page_info_model.dart';
-import 'package:flutter_issue_tracker/issue_tracker/domain/entities/labels.dart';
+import 'package:flutter_issue_tracker/issue_tracker/domain/entities/assignable_users.dart';
 
-class LabelsModel {
-  LabelsModel({
+class AssignableUsersModel {
+  AssignableUsersModel({
     this.pageInfo,
-    this.totalCount,
     this.nodes,
   });
 
-  LabelsModel.fromJson(dynamic json) {
+  AssignableUsersModel.fromJson(dynamic json) {
     pageInfo = json['pageInfo'] != null
         ? PageInfoModel.fromJson(json['pageInfo'])
         : null;
-    totalCount = json['totalCount'] as int?;
     if (json['nodes'] != null) {
       nodes = [];
       json['nodes'].forEach((dynamic v) {
-        nodes?.add(LabelNodeModel.fromJson(v));
+        nodes?.add(AssignableUserNodeModel.fromJson(v));
       });
     }
   }
 
   PageInfoModel? pageInfo;
-  int? totalCount;
-  List<LabelNodeModel>? nodes;
+  List<AssignableUserNodeModel>? nodes;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     if (pageInfo != null) {
       map['pageInfo'] = pageInfo?.toJson();
     }
-    map['totalCount'] = totalCount;
     if (nodes != null) {
       map['nodes'] = nodes?.map((v) => v.toJson()).toList();
     }
     return map;
   }
 
-  Labels toEntity() => Labels(
+  AssignableUsers toEntity() => AssignableUsers(
     nodes: nodes,
-    totalCount: totalCount,
     pageInfo: pageInfo,
   );
 }
