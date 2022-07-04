574a8ae98c99565e24c9178489023a4dec4504ef
jisjoemathew91 <jisjoemathew91@gmail.com>
2022-07-04 19:21:50 +0530

    Add color constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xFF001F2A);
+  static const Color fadeGray = Color(0xFFF0F1F3);
+  static const Color gray = Color(0xFFDCE3E8);
+  static const Color green = Color(0xFF238636);
+  static const Color red = Color(0xFFC00011);
+  static const Color darkBlue = Color(0xFF004EB3);
+  static const Color lightBlue = Color(0xFF3FB5F3);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color darkGray = Color(0xFF7C7F84);
+  static const Color violet = Color(0xFF8957e5);
+  static const Color closedGrey = Color(0xFF6e7681);
+}
